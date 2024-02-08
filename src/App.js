b89e669eaf10479e07742d3fa9b53891768b4ea2
augustinestom12 <augustinestom85@gmail.com>
2024-02-09 00:03:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [quote, setQuote] = useState("Click below to get a new quote");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      setQuote(data.content);
+      setAuthor(data.author);
+    };
+
+    fetchQuote();
+  }, []);
+
+  const handleClick = async () => {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mx-auto p-4">
+      <h2 className="text-3xl text-center mt-6">Random Quotes</h2>
+      <div className="mt-4 ">
+        <blockquote>
+          <p className="text-lg   text-center font-medium">{quote}</p>
+          <p className="  text-center italic ">— {author}</p>
+        </blockquote>
+      </div>
+      <div className="flex justify-center">
+        <button
+          onClick={handleClick}
+          className="mt-4 py-2 px-4 text-center bg-green-500 text-white rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
         >
-          Learn React
-        </a>
-      </header>
+          New Quote
+        </button>
+      </div>
     </div>
   );
 }
